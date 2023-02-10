a8d5d1e78f5edc37ed6e86ea1ea324d9ea0afef7
Luke Hopkins <2447720h@student.gla.ac.uk>
2023-02-10 16:48:13 +0000

    more

--- hcs/urls.py
+++ hcs/urls.py
@@ -20,4 +20,6 @@ from hcs_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
+    path('begin-experiment/', views.randomExperiment, name="random_experiment"),
+
 ]
--- hcs_app/views.py
+++ hcs_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from random import randrange
 # Create your views here.
+
 def homepage(request):
-    
     return render(request, "homepage.html")
+
+
+def randomExperiment(request):
+    pages = ['emoji.html', 'alphanumeric.html', 'emojiandalpha.html']
+    random_page = pages[randrange(3)]
+
+    return render(request, random_page)
 

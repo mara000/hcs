fa4ba1b769b797393adc666a3803a8cbef70c6b8
Luke Hopkins <2447720h@student.gla.ac.uk>
2023-02-10 16:31:48 +0000

    homepage

--- hcs_app/views.py
+++ hcs_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     
-    return render(request, "alphanumeric.html")
+    return render(request, "homepage.html")
 
